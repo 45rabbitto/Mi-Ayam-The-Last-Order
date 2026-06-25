fbfca7bfde832b20963aa38113c198f5c0195043
indhana11 <indhana04@gmail.com>
2026-06-25 11:18:48 +0700

    indhana

--- scripts/interactables/pickup_item.gd
+++ scripts/interactables/pickup_item.gd
@@ -3,9 +3,8 @@ extends Interactable
 @export var item_name : String = ""
 
 func interact():
-
 	print("ITEM DIAMBIL :", item_name)
-
-	Inventory.add_item(self)
-
+	
+	InventoryManager.add_item(self)
+	
 	queue_free()
